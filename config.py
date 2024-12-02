781360caa259bc95233bc9072eebb7764be69168
Mehedi <mehedi.hasan@rwe.com>
2024-12-02 11:50:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-client_id = "ChgaocQB40_j2iYnOIfnpw"      # Your client ID
-client_secret = "Pe44HWcC3AnfE8bH1pil6Vl5bjm3ZA"  # Your secret
+client_id = "Your client ID"      # Your client ID
+client_secret = "Your secret code"  # Your secret
 username = "Your_user_name"    # Your Reddit username
 password = "Your_password"    # Your Reddit password
