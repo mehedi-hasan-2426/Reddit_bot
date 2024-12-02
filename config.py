76b4f7d2564bd452bf537159b41ebf62207c82e8
Mehedi <mehedi.hasan@rwe.com>
2024-12-02 11:55:59 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-client_id = "Your client ID"      # Your client ID
-client_secret = "Your secret code"  # Your secret
-username = "Your_user_name"    # Your Reddit username
-password = "Your_password"    # Your Reddit password
